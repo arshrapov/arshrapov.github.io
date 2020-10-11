c3a2a664dbdb775463e307cb5e215d0cf0e448bb
Raphael <ashrapov.rafa@yandex.com>
2020-10-11 07:56:35 +0500

    Now all is done, but need a adaptive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+const all_button = $('.caourusel__item__all');
+const web_button = $('.caourusel__item__web');
+const bot_button = $('.caourusel__item__bot');
+
 function ibg() {
   $.each($('.ibg'), function(index, val) {
     if($(this).find('img').length> 0){
@@ -7,3 +11,17 @@ function ibg() {
 }
 
 ibg();
+all_button.click (function() {
+  $('.portfolio__item__web').css('display', 'block');
+  $('.portfolio__item__bot').css('display', 'block');
+})
+
+web_button.click (function() {
+  $('.portfolio__item__web').css('display', 'block');
+  $('.portfolio__item__bot').css('display', 'none');
+})
+
+bot_button.click (function() {
+  $('.portfolio__item__web').css('display', 'none');
+  $('.portfolio__item__bot').css('display', 'block');
+})
